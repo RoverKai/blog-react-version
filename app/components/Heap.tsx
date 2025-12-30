a8826c542e4fb0a98eb672e75cd2c66fef60752d
roverkai <2196140098@qq.com>
2025-12-30 15:37:24 +0800

    feat: add Heap component

---
+++ app/components/Heap.tsx
@@ -0,0 +1,22 @@
+import { getAddressFromKey } from "~/utils/MemoryModelUtil";
+import type { ReactNode } from "react";
+
+type HeapProps = {
+  memoryMapKey: string,
+  children: ReactNode,
+}
+
+const Heap = ({children, memoryMapKey}:HeapProps) => {
+  return (
+    <div className={`absolute h-full w-full p-4`}>
+      <div className="text-xs text-gray-500 ">
+        {getAddressFromKey(memoryMapKey)}
+      </div>
+      <div className="flex flex-col *:py-2">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Heap;

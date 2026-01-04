b2a393abe3cbd2439d95997ed0e58ad459e4ddf7
roverkai <2196140098@qq.com>
2026-01-04 13:10:33 +0800

    feat: add chat api streamChat

---
+++ app/api/Chat.ts
@@ -0,0 +1,33 @@
+import type { ChatMessage, ChatRequest } from "~/types/Chat"
+
+export const streamChat = async (
+  messages: ChatMessage[],
+  onChunk: (chunk: string) => void,
+  signal?: AbortSignal
+) => {
+  const res = await fetch("http://localhost:8080/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      messages,
+    }),
+    signal,
+  });
+
+  if (!res.body) return;
+
+  const reader = res.body.getReader();
+  const decoder = new TextDecoder("utf-8");
+
+  while (true) {
+    if (signal?.aborted) break;
+    const { value, done } = await reader.read();
+    if (done) break;
+
+    const chunk = decoder.decode(value, { stream: true });
+    console.log("收到chunk", chunk)
+    onChunk(chunk);
+  }
+};
